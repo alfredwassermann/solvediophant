e702759928d618c53a4376224fedf910c00732ef
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2016-05-12 13:00:41 +0200

    Put constants into const.h

---
+++ const.h
@@ -0,0 +1,16 @@
+#ifndef _LLLCONST_H
+#define _LLLCONST_H
+
+#define DEEPINSERT 1
+#define DEEPINSERT_CONST 50
+#define VERBOSE 1
+
+#define GIVENS 1
+#define LASTLINESFACTOR "1000000" /* "100000000" */
+#define EPSILON 0.000001      /* 0.0001  */
+#define LLLCONST_LOW  0.70 /* 0.75*/
+#define LLLCONST_HIGH 0.99    /* 0.99 */
+#define LLLCONST_HIGHER 0.999
+#define ETACONST 0.51
+
+#endif
--- dio2.c
+++ dio2.c
@@ -8,6 +8,7 @@
 #include <math.h>
 #include <gmp.h>
 #include "dio2.h"
+#include "const.h"
 
 #if defined(USEBLAS)
     #define BLAS 1
@@ -19,18 +20,6 @@
     #include "OpenBLAS/common.h"
     #include "OpenBLAS/cblas.h"
 #endif
-
-#define DEEPINSERT 1
-#define DEEPINSERT_CONST 50
-#define VERBOSE 1
-
-#define GIVENS 1
-#define LASTLINESFACTOR "1000000" /* "100000000" */
-#define EPSILON 0.000001      /* 0.0001  */
-#define LLLCONST_LOW  0.70 /* 0.75*/
-#define LLLCONST_HIGH 0.99    /* 0.99 */
-#define LLLCONST_HIGHER 0.999
-#define ETACONST 0.51
 
 /**
  * global variables
