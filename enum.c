09c6f16943db11767e519df26fffcccfd76d2994
alfred <alfred.wassermann@uni-bayreuth.de>
2019-07-12 16:38:14 +0200

    Enlarge enum_data array

--- enum.c
+++ enum.c
@@ -41,7 +41,7 @@ solution_t solution;
 /**
  * Globals for enumeration
  */
-int MAX_DUAL_BOUNDS = 1024;
+int MAX_DUAL_BOUNDS = 8192; //1024;
 int level_max;
 long loops;
 
@@ -247,7 +247,7 @@ int enumLevel(enum_level_t* enum_data, lattice_t* lattice,
             }
 
             if (ed->num >= MAX_DUAL_BOUNDS) {
-                fprintf(stderr, "enum_data too small! Exit\n");
+                fprintf(stderr, "enum_data too small! Exit\n", ed->num);
                 fflush(stderr);
                 exit(1);
             }
